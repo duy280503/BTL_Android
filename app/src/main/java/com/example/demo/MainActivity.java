8a710684fd2de6ee4f6069a0bb7c38fdad7745fe
luukientruong <luukientruong@gmail.com>
2024-10-20 18:08:26 +0700

    loc ngu

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
+//loc ngu vl
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerKhamPha, recyclerNgheGanDay, recyclerChuDe;
